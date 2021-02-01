c92549cb0397e16b35d729094104cf0e205c935c
Sylwia Zielińska <sylwiazielinska@yahoo.com>
2021-02-01 20:47:01 +0100

    Add loop

--- modul3.2a.py
+++ modul3.2a.py
@@ -2,3 +2,9 @@ shopping_dict = {
     "warzywniak": ["marchew", "seler", "rukola"],
     "piekarnia": ["bułki", "chleb", "pączek"]
 }
+print("Lista Zakupów:")
+licznik = 0
+
+for shop in shopping_dict.keys():
+    products = [product.capitalize() for product in shopping_dict[shop]]
+    licznik += len(products)
