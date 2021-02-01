9bbc763450a8d4577961c87961da31c9a891ffae
Sylwia Zielińska <sylwiazielinska@yahoo.com>
2021-02-01 20:48:41 +0100

    Add summary

--- modul3.2a.py
+++ modul3.2a.py
@@ -8,3 +8,5 @@ licznik = 0
 for shop in shopping_dict.keys():
     products = [product.capitalize() for product in shopping_dict[shop]]
     licznik += len(products)
+    print(f"Idę do {shop.capitalize()}, kupuję tu następujące rzeczy: {products}") 
+print(f"W sumie kupuję {licznik} produktów.")
